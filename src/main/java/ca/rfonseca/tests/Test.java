40be4371ed387a880cd268124f245729b077dbff
Rafael Fonseca <rfonseca85@yahoo.ca>
2016-06-23 14:35:37 -0400

    Add files via upload

---
+++ src/main/java/ca/rfonseca/tests/Test.java
@@ -0,0 +1,9 @@
+package ca.rfonseca.tests;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+}
---
+++ src/test/java/ca/rfonseca85/tests/Test.java
@@ -0,0 +1,10 @@
+package ca.rfonseca85.tests;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+}
